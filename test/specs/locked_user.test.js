41324f2d9222eebb652b87770a89cc3bf7848317
HimaniDas <himani.das1998@gmail.com>
2025-05-08 20:32:47 +0600

    Added locked user test

---
+++ test/specs/locked_user.test.js
@@ -0,0 +1,8 @@
+const LoginPage = require('../pages/loginPage');  
+
+describe('Locked User Test Suite', () =>{  
+  it('should show error for locked_out_user', async () =>{  
+    await LoginPage.login('locked_out_user', 'secret_sauce');  
+    await expect(LoginPage.errorMessage).toHaveText('Epic sadface: Sorry, this user has been locked out.');  
+  });  
+});  
