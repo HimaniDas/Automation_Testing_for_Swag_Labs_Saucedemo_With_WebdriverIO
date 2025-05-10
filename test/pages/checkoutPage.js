6ce6d7cb3802608ac7ecca9dbe890228d25c4592
HimaniDas <himani.das1998@gmail.com>
2025-05-10 20:42:01 +0600

    added checkout page

---
+++ test/pages/checkoutPage.js
@@ -0,0 +1,35 @@
+class CheckoutPage {  
+    get firstName(){ 
+        return $('#first-name'); 
+    }  
+    get lastName(){ 
+        return $('#last-name'); 
+    }  
+    get postalCode(){ 
+        return $('#postal-code'); 
+    }  
+    get continueBtn(){ 
+        return $('#continue'); 
+    }  
+    get itemTotal(){ 
+        return $('.summary_subtotal_label'); 
+    }  
+    get totalPrice(){ 
+        return $('.summary_total_label'); 
+    }  
+    get finishBtn(){ 
+        return $('#finish'); 
+    } 
+    async fillInfo(firstName, lastName, zip) {  
+      await this.firstName.setValue(firstName);  
+      await this.lastName.setValue(lastName);  
+      await this.postalCode.setValue(zip);  
+      await this.continueBtn.click();  
+    }  
+  
+    async finishCheckout(){  
+      await this.finishBtn.click();  
+    }  
+  }  
+  
+  module.exports = new CheckoutPage();
