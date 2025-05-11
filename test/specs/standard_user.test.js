d5a7bbf6656c7f38269e56023ebf9e2e22d5d107
HimaniDas <himani.das1998@gmail.com>
2025-05-11 23:55:29 +0600

    Updated standard user test verification

--- test/specs/standard_user.test.js
+++ test/specs/standard_user.test.js
@@ -1,3 +1,4 @@
+
 const LoginPage = require('../pages/loginPage');  
 const ProductsPage = require('../pages/productPage');  
 const CheckoutPage = require('../pages/checkoutPage');  
@@ -5,15 +6,19 @@ const CheckoutPage = require('../pages/checkoutPage');
 describe('Standard User Test Suite', () =>{  
   it('should complete 3-item purchase', async ()=>{  
     await LoginPage.login('standard_user', 'secret_sauce');  
-     await browser.pause(5000);
+    await browser.pause(5000);
     await ProductsPage.addThreeItems();
     await ProductsPage.goToCart(); 
     //await $('#checkout').click(); 
     await ProductsPage.checkout();
-    await browser.pause(5000);
     await CheckoutPage.fillInfo('Himani', 'Das', '23451');  
     await expect(CheckoutPage.totalPrice).toHaveText('Total: $60.45');  
     await CheckoutPage.finishCheckout(); 
+    await expect(CheckoutPage.successMessage).toHaveText('Thank you for your order!');
+    await ProductsPage.backHome(); 
+    await ProductsPage.menu(); 
+    await ProductsPage.reset();
+    await ProductsPage.logout();
     await browser.pause(1000); 
   });  
 });  
