c3089f7c3041ca3d859438576dc2840db5384c63
HimaniDas <himani.das1998@gmail.com>
2025-05-11 23:56:07 +0600

    Updated performance user test verification

--- test/specs/performance_user.test.js
+++ test/specs/performance_user.test.js
@@ -9,12 +9,16 @@ describe('Performance User Test Suite', () =>{
     await ProductsPage.sortZA(); 
     await ProductsPage.addOneItem(); 
     await ProductsPage.goToCart();
-    //await $('#checkout').click();  
-     await ProductsPage.checkout();
-     await browser.pause(10000);
+    await ProductsPage.checkout();
+    await browser.pause(5000);
     await CheckoutPage.fillInfo('Himani', 'Das', '23451');  
-    await browser.pause(10000);
     await expect(CheckoutPage.totalPrice).toHaveText('Total: $17.27');  
     await CheckoutPage.finishCheckout();
+    await expect(CheckoutPage.successMessage).toHaveText('Thank you for your order!');
+    await ProductsPage.backHome();
+    await ProductsPage.menu();
+    await ProductsPage.reset();
+    await ProductsPage.logout();
+    
   });  
 }); 
