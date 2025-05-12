38ab9902514bb3d0978ad840dd766fba37ce4b7d
HimaniDas <himani.das1998@gmail.com>
2025-05-12 20:40:41 +0600

    Updated Checkout page with full requirements

--- test/specs/performance_user.test.js
+++ test/specs/performance_user.test.js
@@ -6,6 +6,8 @@ describe('Performance User Test Suite', () =>{
   it('should complete Z-A sorted purchase', async () =>{  
     await LoginPage.login('performance_glitch_user', 'secret_sauce');
     await browser.pause(10000);
+    await ProductsPage.menu();
+    await ProductsPage.reset();
     await ProductsPage.sortZA(); 
     await ProductsPage.addOneItem(); 
     await ProductsPage.goToCart();
@@ -14,11 +16,14 @@ describe('Performance User Test Suite', () =>{
     await CheckoutPage.fillInfo('Himani', 'Das', '23451');  
     await expect(CheckoutPage.totalPrice).toHaveText('Total: $17.27');  
     await CheckoutPage.finishCheckout();
+});
+
+  it('should verify successful order message', async () => {
     await expect(CheckoutPage.successMessage).toHaveText('Thank you for your order!');
     await ProductsPage.backHome();
     await ProductsPage.menu();
     await ProductsPage.reset();
     await ProductsPage.logout();
-    
-  });  
-}); 
+    await browser.pause(1000);
+  });
+});
--- test/specs/standard_user.test.js
+++ test/specs/standard_user.test.js
@@ -3,22 +3,26 @@ const LoginPage = require('../pages/loginPage');
 const ProductsPage = require('../pages/productPage');  
 const CheckoutPage = require('../pages/checkoutPage');  
 
-describe('Standard User Test Suite', () =>{  
-  it('should complete 3-item purchase', async ()=>{  
-    await LoginPage.login('standard_user', 'secret_sauce');  
+describe('Standard User Test Suite', () => {
+  it('should complete 3-item purchase', async () => {
+    await LoginPage.login('standard_user', 'secret_sauce');
     await browser.pause(5000);
+    await ProductsPage.menu();
+    await ProductsPage.reset();
     await ProductsPage.addThreeItems();
-    await ProductsPage.goToCart(); 
-    //await $('#checkout').click(); 
+    await ProductsPage.goToCart();
     await ProductsPage.checkout();
-    await CheckoutPage.fillInfo('Himani', 'Das', '23451');  
-    await expect(CheckoutPage.totalPrice).toHaveText('Total: $60.45');  
-    await CheckoutPage.finishCheckout(); 
+    await CheckoutPage.fillInfo('Himani', 'Das', '23451');
+    await expect(CheckoutPage.totalPrice).toHaveText('Total: $60.45');
+    await CheckoutPage.finishCheckout();
+  });
+
+  it('should verify successful order message', async () => {
     await expect(CheckoutPage.successMessage).toHaveText('Thank you for your order!');
-    await ProductsPage.backHome(); 
-    await ProductsPage.menu(); 
+    await ProductsPage.backHome();
+    await ProductsPage.menu();
     await ProductsPage.reset();
     await ProductsPage.logout();
-    await browser.pause(1000); 
-  });  
-});  
+    await browser.pause(1000);
+  });
+});
