329e88b2986c18c49e509ea4b4afe8aa82fc3235
HimaniDas <himani.das1998@gmail.com>
2025-05-16 12:16:51 +0600

    Updated standard user test verification

--- test/specs/standard_user.test.js
+++ test/specs/standard_user.test.js
@@ -1,27 +1,31 @@
-
-const LoginPage = require('../pages/loginPage');  
-const ProductsPage = require('../pages/productPage');  
-const CheckoutPage = require('../pages/checkoutPage');  
+const LoginPage = require('../pages/loginPage');
+const ProductsPage = require('../pages/productPage');
+const CheckoutPage = require('../pages/checkoutPage');
 
 describe('Standard User Test Suite', () => {
-  it('should complete 3-item purchase', async () => {
-    await LoginPage.login('standard_user', 'secret_sauce');
-    await browser.pause(5000);
-    await ProductsPage.menu();
-    await ProductsPage.reset();
-    await ProductsPage.addThreeItems();
-    await ProductsPage.goToCart();
-    await ProductsPage.checkout();
-    await CheckoutPage.fillInfo('Himani', 'Das', '23451');
-    await expect(CheckoutPage.totalPrice).toHaveText('Total: $60.45');
-    await CheckoutPage.finishCheckout();
-  });
-  it('should verify successful order message', async () => {
-    await expect(CheckoutPage.successMessage).toHaveText('Thank you for your order!');
-    await ProductsPage.backHome();
-    await ProductsPage.menu();
-    await ProductsPage.reset();
-    await ProductsPage.logout();
-    await browser.pause(1000);
-  });
+    it('should complete dynamic product purchase and verify total price', async () => {
+        await LoginPage.login('standard_user', 'secret_sauce');
+        await browser.pause(5000);
+        await ProductsPage.menu();
+        await ProductsPage.reset();
+        //await ProductsPage.sortBy('Price (low to high)');
+        const expectedTotal = await ProductsPage.addItemsByCount(3);
+        await ProductsPage.goToCart();
+        await ProductsPage.checkout();
+        await CheckoutPage.fillInfo('Himani', 'Das', '23451');
+        const itemTotalText = await CheckoutPage.itemTotal.getText();
+        const itemTotal = parseFloat(itemTotalText.replace('Item total: $', ''));
+        expect(itemTotal).toBeCloseTo(parseFloat(expectedTotal), 2);
+        await CheckoutPage.finishCheckout();
+         });
+    it('should verify successful order message', async () => {
+        //await expect(CheckoutPage.successMessage).toBeDisplayed({ timeout: 5000 });
+        await expect(CheckoutPage.successMessage).toHaveText('Thank you for your order!');
+        await ProductsPage.backHome();
+        await ProductsPage.menu();
+        await ProductsPage.reset();
+        await ProductsPage.logout();
+        await browser.pause(1000);
+    });
 });
+
