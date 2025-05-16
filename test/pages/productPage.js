58fb323561a54b5c77d27fb4998885b928020d09
HimaniDas <himani.das1998@gmail.com>
2025-05-16 12:13:57 +0600

    Updated product page with full requirements

--- test/pages/productPage.js
+++ test/pages/productPage.js
@@ -1,68 +1,99 @@
+class ProductsPage {
+    get inventoryItems() {
+        return $$('.inventory_item');
+    }
 
-class ProductsPage {  
-  get backpackAddBtn(){ 
-    return $('#add-to-cart-sauce-labs-backpack'); 
-  }  
-  get bikeLightAddBtn(){ 
-    return $('#add-to-cart-sauce-labs-bike-light'); 
-  }  
-  get tshirtAddBtn(){ 
-    return $('#add-to-cart-sauce-labs-bolt-t-shirt'); 
-  }   
-  get redTShirtAddBtn(){ 
-    return $('[data-test="add-to-cart-test.allthethings()-t-shirt-(red)"]');
+    get cartBadge() {
+        return $('#shopping_cart_container');
+    }
+
+    get checkoutBtn() {
+        return $('[data-test="checkout"]');
+    }
+
+    get backhomeBtn() {
+        return $('[data-test="back-to-products"]');
+    }
+
+    get menuBtn() {
+        return $('#react-burger-menu-btn');
+    }
+
+    get resetBtn() {
+        return $('[data-test="reset-sidebar-link"]');
+    }
+
+    get logoutBtn() {
+        return $('[data-test="logout-sidebar-link"]');
+    }
+
+    get sortDropdown() {
+        return $('.product_sort_container');
+    }
+    
+    get redTShirtAddBtn(){ 
+        return $('[data-test="add-to-cart-test.allthethings()-t-shirt-(red)"]');
+    }
+
+    async sortBy(option) {
+        await this.sortDropdown.selectByVisibleText(option);
+    }
+
+    async addItemsByCount(count) {
+        const itemButtons = await $$('[data-test^="add-to-cart-"]');
+        const itemPrices = await $$('div.inventory_item_price');
+        // Combine buttons with prices
+        const items = [];
+        for (let i = 0; i < itemButtons.length; i++) {
+            const priceText = await itemPrices[i].getText();
+            const price = parseFloat(priceText.replace('$', ''));
+            items.push({ button: itemButtons[i], price });
+        }
+        // Shuffle and pick N random items
+        for (let i = items.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [items[i], items[j]] = [items[j], items[i]];
+        }
+
+        let total = 0;
+        for (let i = 0; i < count; i++) {
+            await items[i].button.click();
+            total += items[i].price;
+        }
+
+        return total.toFixed(2);
+    }
+    
+    async sortZA() {  
+        await this.sortDropdown.selectByAttribute('value', 'za');  
+    } 
+    async addOneItem(){
+        await this.redTShirtAddBtn.click();
+  }
+
+    async goToCart() {
+        await this.cartBadge.click();
+    }
+
+    async checkout() {
+        await this.checkoutBtn.click();
+    }
+
+    async backHome() {
+        await this.backhomeBtn.click();
+    }
+
+    async menu() {
+        await this.menuBtn.click();
+    }
+
+    async reset() {
+        await this.resetBtn.click();
+    }
+
+    async logout() {
+        await this.logoutBtn.click();
+    }
 }
-  get cartBadge(){
-     return $('#shopping_cart_container'); 
-    }  
-  get sortDropdown(){ 
-    return $('.product_sort_container'); 
-  }  
-  get checkoutBtn(){ 
-    return $('[data-test="checkout"]');
-  } 
-  get backhomeBtn(){ 
-    return $('[data-test="back-to-products"]');
-  }  
-  get menuBtn(){
-     return $('[id="react-burger-menu-btn"]');
-  }
-  get resetBtn(){
-     return $('[data-test="reset-sidebar-link"]');
-  }
-  get logoutBtn(){
-     return $('[data-test="logout-sidebar-link"]');
-  }
 
-  async addThreeItems(){  
-    await this.backpackAddBtn.click();  
-    await this.bikeLightAddBtn.click();
-    await this.tshirtAddBtn.click();  
-  }  
-  async checkout(){
-    await this.checkoutBtn.click();
-  }
-  async sortZA() {  
-    await this.sortDropdown.selectByAttribute('value', 'za');  
-  }  
-  async addOneItem(){
-    await this.redTShirtAddBtn.click();
-  }
-  async goToCart() {  
-    await this.cartBadge.click(); 
-  }
-  async backHome() {  
-    await this.backhomeBtn.click(); 
-  }
-async menu() {  
-    await this.menuBtn.click(); 
-  }
-  async reset() {  
-    await this.resetBtn.click(); 
-  }
-  async logout() {  
-    await this.logoutBtn.click(); //logout
-  }
-  
-}  
 module.exports = new ProductsPage();
