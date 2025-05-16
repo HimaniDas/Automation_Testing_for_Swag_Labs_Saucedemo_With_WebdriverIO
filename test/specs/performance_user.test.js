6a855f1861c82b5bcc0a82f7dd43a0ad30e39967
HimaniDas <himani.das1998@gmail.com>
2025-05-16 12:15:51 +0600

    Updated performance user test verification

--- test/specs/performance_user.test.js
+++ test/specs/performance_user.test.js
@@ -1,3 +1,4 @@
+
 const LoginPage = require('../pages/loginPage');  
 const ProductsPage = require('../pages/productPage');  
 const CheckoutPage = require('../pages/checkoutPage');  
@@ -6,23 +7,21 @@ describe('Performance User Test Suite', () =>{
   it('should complete Z-A sorted purchase', async () =>{  
     await LoginPage.login('performance_glitch_user', 'secret_sauce');
     await browser.pause(10000);
-    await ProductsPage.menu();
-    await ProductsPage.reset();
     await ProductsPage.sortZA(); 
     await ProductsPage.addOneItem(); 
     await ProductsPage.goToCart();
+    //await $('#checkout').click();  
     await ProductsPage.checkout();
     await browser.pause(5000);
     await CheckoutPage.fillInfo('Himani', 'Das', '23451');  
     await expect(CheckoutPage.totalPrice).toHaveText('Total: $17.27');  
     await CheckoutPage.finishCheckout();
-});
-  it('should verify successful order message', async () => {
+    });
+    it('should verify successful order message', async () => {
     await expect(CheckoutPage.successMessage).toHaveText('Thank you for your order!');
     await ProductsPage.backHome();
     await ProductsPage.menu();
     await ProductsPage.reset();
     await ProductsPage.logout();
-    await browser.pause(1000);
-  });
-});
+  });  
+}); 
